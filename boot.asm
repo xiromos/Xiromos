ea8152547690309ee9537fb4b867c2b71d41228e
technodon <subwayheimerwender@icloud.com>
2026-02-16 18:23:13 +0300

    added an extra functio:   boot.asm 	modified:   boot.bin

--- boot.asm
+++ boot.asm
@@ -1,41 +1,49 @@
 [org 0x7c00]
 bits 16
 
-main:
+start:
     cli
-    mov ax, 0
+    xor ax, ax
     mov ds, ax      ;start adress of the data segment
     mov es, ax      ;start point of the extra segment
     mov ss, ax
     mov sp, 0x7c00
-    mov si, os_boot_msg
     sti
-    call print   
+    mov si, os_boot_msg
+    call print_loop
+    call key_input
+    mov si, booting_msg
+    call print_loop
+
     hlt
+
 
 halt:
     jmp halt
 
-print:
-    push si
-    push ax
-    push bx
+key_input:
+    xor ah, ah
+    int 0x16        ;keyboard interrupt
+    ;AL = key is pressed
+    mov ah, 0x0e
+    int 0x10
+    ret  
+
 
 print_loop:
-    lodsb
+    lodsb           ;loads the next byte of os_boot_msg
     or al, al
     jz done_print
     mov ah, 0x0e
     mov bh, 0
     int 0x10
     jmp print_loop
+
     
 done_print:
-    pop bx
-    pop si
-    pop ax
-    ret
+    ret       
 
-os_boot_msg: db 'Press Any Key To Boot...', 0x0d, 0x0a
+os_boot_msg: db 'Press Any Key To Boot...', 0
+booting_msg: db 'Key Pressed. Booting...', 0
 times 510 - ($ - $$) db 0
 dw 0xAA55
