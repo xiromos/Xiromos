18b509169ccab3398cb269142de0e147f89f8171
technodon <subwayheimerwender@icloud.com>
2026-02-17 17:24:58 +0300

    Tried to fix... nothin is working

--- boot.asm
+++ boot.asm
@@ -6,19 +6,52 @@ start:
     xor ax, ax
     mov ds, ax      ;start adress of the data segment
     mov es, ax      ;start point of the extra segment
+    mov ax, 0x0000
     mov ss, ax
     mov sp, 0x7c00
+    mov [boot_drive], dl
     sti
     mov si, os_boot_msg
     call print_loop
     call key_input
     mov si, booting_msg
     call print_loop
+    call load_kernel
+
+    mov ah, 0x0E
+    mov al, 'J'
+    int 0x10
+
+    jmp 0x1000:0x0000
     hlt
 
 
 halt:
     jmp halt
+
+load_kernel:
+    mov ah, 0x02            ; BIOS read sector (function number)
+    mov al, 1               ; Anzahl Sektoren (ANPASSEN!)
+    mov ch, 0               ; Cylinder
+    mov dh, 0               ; Head
+    mov cl, 2               ; ab Sektor 2
+    mov dl, [boot_drive]    ; erste Festplatte
+    mov bx, 0x0000          ; Offset
+    mov ax, 0x1000
+    mov es, ax              ; Zielsegment 0x1000
+    int 0x13
+    ;test
+    mov ah, 0x0E
+    mov al, 'L'
+    int 0x10
+    ;test-end
+    jc disk_error
+    ret
+
+disk_error:
+    mov si, disk_error_msg
+    call print_loop
+    jmp $
 
 key_input:
     xor ah, ah
@@ -42,7 +75,9 @@ print_loop:
 done_print:
     ret       
 
+boot_drive db 0
 os_boot_msg: db 'Press Any Key To Boot...', 0x0d, 0x0a, 0
 booting_msg: db 0x0d, 0x0a, 'Key Pressed. Booting...', 0
+disk_error_msg: db 'Disk Error Occured', 0
 times 510 - ($ - $$) db 0
 dw 0xAA55            ;v1.0
