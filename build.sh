8cda977be20d275d6cde1ba4b1df42967ecf7cef
Technodon <subwayheimerwender@icloud.com>
2026-02-24 19:54:29 +0300

    added hwinfo

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ printf "${GREEN}Succesfully compiled files!${RESET}\n"
 dd if=/dev/zero of=disk.img bs=512 count=16
 dd if=boot.bin of=disk.img conv=notrunc
 dd if=kernel.bin of=disk.img bs=512 seek=1 conv=notrunc
-dd if=calc.bin of=disk.img bs=512 seek=4 conv=notrunc
+dd if=calc.bin of=disk.img bs=512 seek=6 conv=notrunc
 dd if=hwinfo.bin of=disk.img bs=512 seek=9 conv=notrunc
 printf "${GREEN}Copied data to disk image. Loading QEMU...${RESET}\n"
 qemu-system-i386 -hda disk.img -m 512M
