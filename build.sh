b364825e798ae8a2b8b3b0aa70b477328a9ae9ba
Technodon <subwayheimerwender@icloud.com>
2026-03-18 11:18:13 +0300

    NOW WITH FAT16 FILESYSTEM

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 RED="\033[31m"
 GREEN="\033[32m"
 MAGENTA="\033[35m"
@@ -10,21 +8,24 @@ trap 'printf "${RED}Build failed!${RESET}\n"; exit 1' ERR
 
 nasm -f bin boot.asm -o boot.bin
 nasm -f bin kernel.asm -o kernel.bin
-nasm -f bin calc.asm -o calc.bin
-nasm -f bin hwinfo.asm -o hwinfo.bin
 printf "${GREEN}Succesfully compiled files!${RESET}\n"
-dd if=/dev/zero of=disk.img bs=512 count=16
-dd if=boot.bin of=disk.img conv=notrunc
-dd if=kernel.bin of=disk.img bs=512 seek=1 conv=notrunc
-dd if=calc.bin of=disk.img bs=512 seek=6 conv=notrunc
-dd if=hwinfo.bin of=disk.img bs=512 seek=9 conv=notrunc
+dd if=/dev/zero of=disk.img bs=1M count=16
+#mkfs.fat -F 16 disk.img
+#dd if=boot.bin of=disk.img conv=notrunc
+#dd if=kernel.bin of=disk.img bs=512 seek=18 conv=notrunc
+mkdosfs -F 16 disk.img 
+#dd if=boot.bin of=disk.img bs=1 count=11 conv=notrunc
+dd if=boot.bin of=disk.img bs=1 count=450 seek=62 skip=62 conv=notrunc
+mcopy -i disk.img kernel.bin ::KERNEL.BIN
+printf "${MAGENTA}Disk layout:${RESET}\n"
+mdir -i disk.img ::
 printf "${GREEN}Copied data to disk image. Loading QEMU...${RESET}\n"
-qemu-system-i386 -hda disk.img -m 512M
+qemu-system-i386 -hda disk.img
 
 
-#Memory Map
-#Bootloader: 0x7c00
-#Kernel: 0x1000
-#Programs: 0x3000
-#Stack: 0x8000
-#Stackpointer: 0xfffe
+# memory map
+# 0x0800: root directory
+# 0x1300: FATs
+# 0x????: data
+# 0x2000: kernel.bin
+# 0xFFFF: Stack
