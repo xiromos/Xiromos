73681d0c95d5cf7309e688a56ad29ac455f65073
Technodon <subwayheimerwender@icloud.com>
2026-03-28 18:47:07 +0300

    added .txt files

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ nasm -f bin kernel/kernel.asm -o kernel/kernel.bin
 nasm -f bin programs/hwinfo.asm -o programs/hwinfo.bin
 nasm -f bin programs/calc.asm -o programs/calc.bin
 nasm -f bin programs/hello.asm -o programs/hello.bin
+nasm -f bin programs/ascii.asm -o programs/ascii.bin
 nasm -f bin programs/xir.asm -o programs/xir.bin
 printf "${GREEN}Succesfully compiled files!${RESET}\n"
 rm disk.img
@@ -21,28 +22,25 @@ mcopy -i disk.img kernel/kernel.bin ::KERNEL.BIN
 mcopy -i disk.img programs/hwinfo.bin ::HWINFO.BIN
 mcopy -i disk.img programs/calc.bin ::CALC.BIN
 mcopy -i disk.img programs/hello.bin ::HELLO.BIN
+mcopy -i disk.img programs/ascii.bin ::ASCII.BIN
 mcopy -i disk.img programs/xir.bin ::XIR.BIN
-mcopy -i disk.img programs/test.txt ::TEST.TXT
+mcopy -i disk.img txt/hello.txt ::HELLO.TXT
+mcopy -i disk.img txt/fscmds.txt ::FSCMDS.TXT
+mcopy -i disk.img txt/system.txt ::SYSTEM.TXT
 printf "${MAGENTA}Disk layout:${RESET}\n"
 mdir -i disk.img ::
 printf "${GREEN}Copied data to disk image. Loading QEMU...${RESET}\n"
-qemu-system-i386 -hda disk.img -m 2M
+qemu-system-i386 -hda disk.img  -hdb disk2.img -m 2M 
 
 
 # memory map
 # 0x0x0000:0500: root directory
 # 0x0000:0x3999: FAT
 # 0x1000:x0000: kernel.bin
-# 0x2000:0x0000: programs
+# 0x5000:0x0000: programs
 # 0xFFFE: kernel stack
-# 0x0000:0x8000: int 0x20
 
-# TODO
-# -write own emulator
-# -design own CPU
-# -write own AI
-# -write own programming language with compiler
-# -write own assembler
-# -write own CUDA driver
-# -write own BIOS
-# -
+# write on USB stick:
+# lsblk
+# sudo dd if=disk.img of=/dev/sdb bs=4M status=progress conv=fsync
+# sync
