6733355df767e6a140d2377e4d74b956381540f7
Technodon <subwayheimerwender@icloud.com>
2026-04-04 13:29:23 +0300

    nothing

---
+++ programs/ints/0x25.asm
@@ -0,0 +1,6 @@
+;==============================================================================
+;file api for floppy disks
+;AH = 0x01: search a file in root direcory and then copy the content into a buffer      (filename in SI, output = buffer in DI)
+;==============================================================================
+;Copyright (C) 2026 Technodon
+;------------------------------------------------------------------------------
