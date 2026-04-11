219c9eed0e14dd191762215dcca18dda725f68c8
Technodon <subwayheimerwender@icloud.com>
2026-04-11 20:41:14 +0300

    copies .bin files into another folder

--- build.sh
+++ build.sh
@@ -15,37 +15,51 @@ nasm -f bin programs/ascii.asm -o programs/ascii.bin
 nasm -f bin programs/xir.asm -o programs/xir.bin
 nasm -f bin programs/xfetch.asm -o programs/xfetch.bin
 nasm -f bin programs/lsdisk.asm -o programs/lsdisk.bin
-nasm -f bin programs/textedit.asm -o programs/textedit.bin
+nasm -f bin programs/xedit.asm -o programs/xedit.bin
+nasm -f bin programs/xformat.asm -o programs/xformat.bin
+nasm -f bin programs/help.asm -o programs/help.bin
+nasm -f bin programs/font.asm -o programs/font.bin
+nasm -f bin programs/scancode.asm -o programs/scancode.bin
 printf "${GREEN}Succesfully compiled files!${RESET}\n"
-rm disk.img
+cp programs/ascii.bin zprograms/
+cp programs/calc.bin zprograms/
+cp programs/font.bin zprograms/
+cp programs/hello.bin zprograms/
+cp programs/help.bin zprograms/
+cp programs/hwinfo.bin zprograms/
+cp programs/lsdisk.bin zprograms/
+cp programs/scancode.bin zprograms/
+cp programs/xedit.bin zprograms/
+cp programs/xfetch.bin zprograms/
+cp programs/xformat.bin zprograms/
+cp programs/xir.bin zprograms/
+#rm disk.img
 dd if=/dev/zero of=disk.img bs=1M count=16
 mkdosfs -F 16 -v disk.img
 dd if=boot.bin of=disk.img bs=1 count=450 seek=62 skip=62 conv=notrunc
 mcopy -i disk.img kernel/kernel.bin ::KERNEL.BIN
-mcopy -i disk.img programs/hwinfo.bin ::HWINFO.BIN
-mcopy -i disk.img programs/calc.bin ::CALC.BIN
-mcopy -i disk.img programs/hello.bin ::HELLO.BIN
-mcopy -i disk.img programs/ascii.bin ::ASCII.BIN
-mcopy -i disk.img programs/xir.bin ::XIR.BIN
-mcopy -i disk.img programs/xfetch.bin ::XFETCH.BIN
-mcopy -i disk.img programs/lsdisk.bin ::LSDISK.BIN
-mcopy -i disk.img programs/textedit.bin ::EDIT.BIN
-mcopy -i disk.img txt/hello.txt ::HELLO.TXT
-mcopy -i disk.img txt/fscmds.txt ::FSCMDS.TXT
-mcopy -i disk.img txt/system.txt ::SYSTEM.TXT
+mcopy -i disk.img programs/help.bin ::HELP.BIN
+mcopy -i disk.img txt/ ::TXT
+mcopy -i disk.img zprograms/ ::PROGRAMS
+mcopy -i disk.img -s a ::/
+# mattrib -i disk.img +s ::SYSTEM.TXT
 printf "${MAGENTA}Disk layout:${RESET}\n"
 mdir -i disk.img ::
 # mdir -i floppy.img ::
 printf "${GREEN}Copied data to disk image. Loading QEMU...${RESET}\n"
-qemu-system-i386 -hda disk.img -hdb disk2.img -fda floppy.img -cpu 486 #,model_id="AuthenticAMD" # -fdb floppy2.img -m 2M -hdc disk3.img
+qemu-system-i386 -hda disk.img -hdb disk2.img -fda floppy.img -fdb floppy8.img -m 2M # -hdc disk3.img
 # mkdosfs -F 12 -v disk3.img
 # mkdosfs -F 16 -v disk2.img
+
 # memory map
 # 0x0x0000:0500: root directory
 # 0x0000:0x3999: FAT
+# 0x0000:0x7c00: boot code
 # 0x1000:x0000: kernel.bin
 # 0x5000:0x0000: programs
 # 0xFFFE: kernel stack
+# 0x9000:0x0000: directories
+# 0x9000:0x7c00: boot code of external disks
 
 # write on USB stick:
 # lsblk
