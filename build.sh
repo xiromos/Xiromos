2f4243998df17060c9626f6b22fd160311b488cb
Technodon <subwayheimerwender@icloud.com>
2026-05-01 11:19:30 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -33,21 +33,30 @@ cp programs/xedit.bin zprograms/
 cp programs/xfetch.bin zprograms/
 cp programs/xformat.bin zprograms/
 cp programs/xir.bin zprograms/
-#rm disk.img
-dd if=/dev/zero of=disk.img bs=1M count=16
+
+# nasm -f bin ~/Downloads/xmode/kernel/kernel.asm -o ~/Downloads/xmode/kernel/kernel.bin                # Uncomment to use XMODE
+# nasm -f bin ~/Downloads/xmode/programs/help.asm -o ~/Downloads/xmode/programs/help.bin                # Uncomment to use XMODE
+
+
+# rm disk.img
+dd if=/dev/zero of=disk.img bs=1M count=9
 mkdosfs -F 16 -v disk.img
 dd if=boot.bin of=disk.img bs=1 count=450 seek=62 skip=62 conv=notrunc
 mcopy -i disk.img kernel/kernel.bin ::KERNEL.BIN
-mcopy -i disk.img programs/help.bin ::HELP.BIN
+# mcopy -i disk.img ~/Downloads/xmode/kernel/kernel.bin ::XMODE.BIN                                     # Uncomment to use XMODE
+# mcopy -i disk.img programs/help.bin ::HELP.BIN
+mcopy -i disk.img ~/Downloads/xmode/programs/help.bin ::HELP.BIN
 mcopy -i disk.img txt/ ::TXT
 mcopy -i disk.img zprograms/ ::PROGRAMS
 mcopy -i disk.img -s a ::/
-# mattrib -i disk.img +s ::SYSTEM.TXT
+mcopy -i disk.img txt/fscmds.txt ::TEST.TXT
+# mattrib -i disk.img +s ::KERNEL.BIN
+# mattrib -i disk.img +s ::XMODE.BIN
 printf "${MAGENTA}Disk layout:${RESET}\n"
 mdir -i disk.img ::
 # mdir -i floppy.img ::
 printf "${GREEN}Copied data to disk image. Loading QEMU...${RESET}\n"
-qemu-system-i386 -hda disk.img -hdb disk2.img -fda floppy.img -fdb floppy8.img -m 2M # -hdc disk3.img
+qemu-system-i386 -hda disk.img -hdb disk2.img -fda floppy.img -fdb floppy8.img -m 64M # -d int -no-reboot # -hdc disk3.img
 # mkdosfs -F 12 -v disk3.img
 # mkdosfs -F 16 -v disk2.img
 
